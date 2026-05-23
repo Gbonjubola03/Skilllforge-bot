99adc893374bddac0c545a470fd97341a0dc18fe
Gbonjubola03 <148791178+Gbonjubola03@users.noreply.github.com>
2026-05-23 08:29:44 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,10 @@
 import discord
 from discord.ext import commands
+from dotenv import load_dotenv
+import json
+import os
+import re
+
 
 intents = discord.Intents.default()
 intents.message_content = True
